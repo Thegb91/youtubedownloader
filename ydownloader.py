c5d4ea529452e39212b86ba5100411ecd72a08fc
Ahmed <aroui.ahmed@gmail.com>
2017-10-02 23:31:03 +0200

    adding first version of youtube downloader

---
+++ ydownloader.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+
+"""ydownloader.py: Description of what foobar does."""
+
+__author__      = "Thegb91"
+__copyright__   = "Copyright 2017"
+
+import youtube_dl, sys
+
+ydl_opts = {
+    'format': 'bestaudio/best', # choice of quality
+    'extractaudio' : True,      # only keep the audio
+    'audioformat' : "mp3",      # convert to mp3 
+    'outtmpl': '%(title)s.mp3',
+    'noplaylist' : True,}
+for i in range(1, len(sys.argv)):
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([sys.argv[i]])
